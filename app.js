81d485836d9484ef157de2ce0a6e270d2ba16c20
arjunsingh <arjunksingh1027@gmail.com>
2020-12-09 17:24:49 -0700

    fixing api and html routes

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path")
+const uuid = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3030;
@@ -8,9 +12,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+// api routes
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
 
+// post api/notes - receive a new note, add it to db.json, return new note to user
+app.post("/api/notes", function (req, res) {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+// delete notes
+app.delete("/api/notes/:id", function (req, res) {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const deleteNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote));
+    res.json(deleteNote);
+});
+
+// html routes
+// return notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+// return index.html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+
+// port listen
 app.listen(PORT, function () {
     console.log("app is listening on port:" +PORT);
 })
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,34 +5,23 @@ const savedNotes = require("../db/db.json");
 module.exports = function (app) {
     // get api/notes - reads db.json and returns saved notes as JSON
     app.get("/api/notes", function (req, res) {
-        res.json(savedNotes);
+        res.sendFile(path.join(__dirname, "/db/db.json"))
     });
 
     // post api/notes - receive a new note, add it to db.json, return new note to user
     app.post("/api/notes", function (req, res) {
-        // new note to save on the request
-        const note = req.body
-        // generate id for each new note
-        note.id = uuid.v4();
-        // add new note to savedNotes array
-        savedNotes.push(note);
-        // write note to db.json with updated savedNotes array
-        fs.writeFile("./db/db.json", JSON.stringify(savedNotes))
-        // display user created note
-        res.json(note);
+        const notes = JSON.parse(fs.readFileSync(".db/db.json"));
+        const newNotes = req.body;
+        newNotes.id = uuid.v4();
+        notes.push(newNotes);
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+        res.json(notes);
     });
 
-    app.delete("/api/:id", function (req, res) {
-        // query to find id of the note to delete
-        const chosen = req.params.id;
-
-        // read notes from db.json
-        const oldNote = JSON.parse(fs.readFileSync("./db/db.json"));
-
-        // remove note with targeted id
-        const NewNote = JSON.stringify(oldNote.filter((savedNotes) => savedNotes.id !== chosen));
-
-        // rewrite notes to db.json
-        fs.writeFile("./db/db.json", JSON.stringify(NewNote));
+    app.delete("/api/notes/:id", function (req, res) {
+        const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+        const deleteNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+        fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote));
+        res.json(deleteNote);
     });
 };
